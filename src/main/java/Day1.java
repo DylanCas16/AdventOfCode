c2bf98ef98429205c28b93fb9d3370d71d81cea4
DylanCas16 <dylancastrillon@outlook.com>
2025-12-04 23:17:41 +0000

    Day 1 (Part 1) completed.

--- src/main/java/Day1.java
+++ src/main/java/Day1.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class Day1 {
+import java.util.Scanner;
+import static java.lang.Math.*;
+
+public class Day1 {
+    private static final Scanner line = new Scanner(System.in);
+    private static int password = 0;
+    private static int pointer = 50;
+
+    public static void main(String[] args) {
+        while (true) {
+            getNewPointer();
+            adjustPointer();
+            if (pointer == 0) password++;
+            System.out.println(password);
+        }
+    }
+
+    private static void getNewPointer() {
+        String move = line.nextLine();
+        int numberMove = Integer.parseInt(move.substring(1));
+        if (move.startsWith("R")) pointer += numberMove;
+        if (move.startsWith("L")) pointer -= numberMove;
+    }
+
+    private static void adjustPointer() {
+        pointer = floorMod(pointer, 100);
+    }
 }
